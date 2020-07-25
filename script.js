725810bdf453d42d9cc0de9f9c40f846e7f8bcdd
Sarah <soobin.sarah.hwang@gmail.com>
2020-07-24 20:17:53 -0700

    UV index color added

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     const cityContainer = $('#cityWeather');
+    let cities = $(this).attr('data-name');
 
     $('#searchBtn').on('click', function(event){
         event.preventDefault();
         cityContainer.empty();
         let city = $('#cityInput').val().trim();
+        localStorage.setItem('searchHistory', JSON.stringify(city));
         searchWeather(city);
     })
 
@@ -19,12 +21,13 @@ function searchWeather(cityName) {
         
         let date = moment().format('MM/DD/YYYY');
         let city = $('<h2>').text(response.name);
-        let iconimg = $("<img>").attr('src', 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png')
+        let iconimg = $("<img>").attr('src', 'https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png');
         let temperature = $('<p>').text(`Temperature: ${response.main.temp.toFixed(0)}`);
         let humidity = $('<p>').text(`Humidity: ${response.main.humidity}`);
         let windSpeed = $('<p>').text(`Wind Speed: ${response.wind.speed}`);
         
-        cityContainer.append(city, date, iconimg, temperature, humidity, windSpeed);
+        
+        cityContainer.append(city, iconimg, date, temperature, humidity, windSpeed);
     
     let uvURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=ab1f8fef34f0e489ad088b4507c416a5&lat=' + response.coord.lat + '&lon=' + response.coord.lon;
 
@@ -32,28 +35,30 @@ function searchWeather(cityName) {
         url: uvURL,
         method: 'GET'
     }).then(function(response) {
-        let uvindex = response.value;
-        let uv = $('<div>').text(`UV Index: ${response.value}`);
-    
-
-        if (uvindex <= 2) {
-            cityContainer.attr('class', 'low')
-        } else if (uvindex >= 3 || uvindex <= 5) {
-            cityContainer.attr('class', 'moderate')
-        } else if (uvindex >= 6 || uvindex <=7) {
-            cityContainer.attr('class', 'high')
-        } else {
-            cityContainer.attr('class', 'veryHigh')
-        };
-
-        cityContainer.append(uv)
+        let uvIndex = JSON.parse(response.value);
+        let uvElement = $('<span>').text(`UV Index: ${response.value}`);
         
-       
+        if (uvIndex <= 2) {
+            uvElement.css('background-color', 'green')
+        } 
+        if (uvIndex >= 3 || uvIndex <= 5) {
+            uvElement.css('background-color', 'yellow')
+        }  
+        if (uvIndex >= 6 || uvIndex <=7) {
+            uvElement.css('background-color', 'orange')
+        }  
+        if (uvIndex >8) {
+            uvElement.css('background-color', 'red')
+        }
+            
+        cityContainer.append(uvElement);
+        
     })
     
     });
       
-      
+
+
     
 }
 })
